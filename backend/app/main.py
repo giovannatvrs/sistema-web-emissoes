14113ab7990329a6a7032a2e17e71301f7e9b911
giovanna <giovannatvrs@outlook.com>
2026-01-26 07:18:56 -0300

    adicionar endpoint de estatísticas

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
+from itertools import groupby
+
 from fastapi import FastAPI, HTTPException, status, Path, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy import func, asc, desc
+from sqlalchemy import func, asc, desc, extract
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from app.models import Emissao
@@ -108,5 +110,57 @@ def editar_emissao(id: int, emissao: EmissaoUpdate, db: Session = Depends(get_db
     return db_emissao
 
 @app.get("/stats")
-def get_estatisticas():
-    return
+def get_estatisticas(db: Session = Depends(get_db)):
+    volume_total = db.query(func.sum(Emissao.valor)).scalar()
+
+    volume_total_por_ano = db.query(extract('year', Emissao.data).label("ano"),
+                        func.sum(Emissao.valor).label("valor_total")).group_by("ano").order_by("ano").all()
+
+    volume_total_por_ano_mes = db.query(extract('year', Emissao.data).label("ano"), extract('month', Emissao.data).label("mes"),
+            func.sum(Emissao.valor).label("valor_total")).group_by("ano", "mes").order_by(  "ano", "mes").all()
+
+
+    volume_por_tipo = db.query(Emissao.tipo, func.sum(Emissao.valor).label("valor_total")).group_by("tipo").order_by( "tipo").all()
+
+
+    qtd_emissoes_ano = db.query(extract('year', Emissao.data).label("ano"), func.count(Emissao.id).label("qtd_emissoes")).group_by("ano").order_by("ano").all()
+
+    qtd_emissoes_por_ano_mes = db.query(extract('year', Emissao.data).label("ano"), extract('month', Emissao.data).label("mes"),
+        func.count(Emissao.id).label("qtd_emissoes")).group_by("ano", "mes").order_by("ano", "mes").all()
+
+
+    qtd_emissoes_por_tipo = db.query(Emissao.tipo, func.count(Emissao.id).label("qtd_emissoes")).group_by("tipo").order_by("tipo").all()
+
+    qtd_emissoes_por_emissor = db.query(Emissao.emissor, func.count(Emissao.id).label("qtd_emissoes")).group_by("emissor").order_by(func.count(Emissao.id).desc()).all()
+
+    volume_medio = db.query(func.avg(Emissao.valor)).scalar()
+
+    top_5_emissores_por_volume = db.query(Emissao.emissor, func.sum(Emissao.valor).label("valor_total")).group_by(
+        Emissao.emissor).order_by(func.sum(Emissao.valor).desc()).limit(5).all()
+    top_5_emissores_por_qtd = db.query(Emissao.emissor, func.count(Emissao.id).label("qtd_emissoes")).group_by(
+        Emissao.emissor).order_by(func.count(Emissao.id).desc()).limit(5).all()
+    tipos_mais_relevantes_por_volume = db.query(Emissao.tipo, func.sum(Emissao.valor).label("valor_total")).group_by(
+        Emissao.tipo).order_by(func.sum(Emissao.valor).desc()).all()
+
+
+    return {
+        "volume_total": volume_total,
+        "volume_medio": volume_medio,
+        "volume_por_ano": [{"ano": r.ano, "valor_total": r.valor_total} for r in volume_total_por_ano],
+        "volume_por_ano_mes": [{"ano": r.ano, "mes": r.mes, "valor_total": r.valor_total}
+                               for r in volume_total_por_ano_mes],
+        "volume_por_tipo": [{"tipo": r.tipo, "valor_total": r.valor_total}
+                               for r in volume_por_tipo],
+        "qtd_emissoes_por_ano": [{"ano": r.ano, "qtd_emissoes": r.qtd_emissoes}
+                                for r in qtd_emissoes_ano],
+        "qtd_emissoes_por_ano_mes": [{"ano": r.ano, "mes": r.mes, "qtd_emissoes": r.qtd_emissoes}
+                                     for r in qtd_emissoes_por_ano_mes],
+        "qtd_emissoes_por_tipo": [{"tipo": r.tipo, "qtd_emissoes": r.qtd_emissoes}
+                                for r in qtd_emissoes_por_tipo],
+        "maiores_emissores_por_volume": [{"emissor": r.emissor, "valor_total": r.valor_total} for r in
+                                       top_5_emissores_por_volume],
+        "maiores_emissores_por_qtd": [{"emissor": r.emissor, "qtd_emissoes": r.qtd_emissoes} for r in
+                                    top_5_emissores_por_qtd],
+        "tipos_mais_relevantes_por_volume": [{"tipo": r.tipo, "valor_total": r.valor_total} for r in
+                                             tipos_mais_relevantes_por_volume]
+    }
